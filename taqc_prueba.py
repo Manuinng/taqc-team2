9e35edd5129ee867525eb69a0d0fd40c526ac317
Manuinng <manuel.quijadapo@gmail.com>
2025-03-19 12:19:24 -0300

    POM

--- taqc_prueba.py
+++ taqc_prueba.py
@@ -1,1 +1,12 @@
-probando
+from playwright.async_api import async_playwright
+import asyncio
+
+URL = "https://automation-portal-bootcamp.vercel.app"
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page =  await browser.new_page()
+        await page.goto(URL, wait_until="domcontentloaded")
+
+
+        asyncio.run(main()) 

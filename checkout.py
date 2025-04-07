c189b9d159b5cab1f3fd454858697d452c353999
lfazocar <39388875+lfazocar@users.noreply.github.com>
2025-04-07 00:38:08 -0400

    feat: add checkout model

--- checkout.py
+++ checkout.py
@@ -2,6 +2,7 @@ import asyncio
 from playwright.async_api import async_playwright
 from models.automation_portal import AutomationPortal
 from models.cart_sidebar import CartSidebar
+from models.checkout import Checkout
 
 async def checkout():
     async with async_playwright() as p:
@@ -9,8 +10,6 @@ async def checkout():
         page = await browser.new_page()
 
         portal = AutomationPortal(page)
-        cart_sidebar = CartSidebar(page)
-
         await portal.navigate()
         await portal.close_newsletter_popup()
 
@@ -19,9 +18,17 @@ async def checkout():
         # await portal.submit_login_popup()
         # await page.keyboard.press("Escape") # To remove bugged overlay in "My Account" page
 
+        cart_sidebar = CartSidebar(page)
         await cart_sidebar.open()
         await cart_sidebar.click_tos_checkbox()
         await cart_sidebar.go_to_checkout()
+
+        checkout = Checkout(page)
+        await checkout.fill_billing_details("first", "last", "Spain", "city", "address", "phone", "email", "notes")
+        await checkout.apply_discount_code("discount")
+        await checkout.fill_credit_card_details("4242424242424242", "12/12", "123")
+        await checkout.click_tos_checkbox()
+        await checkout.place_order()
 
         await asyncio.sleep(5)
         await browser.close()

84ab1af3aa5bf845518deea37c5833770ceec65b
Manu <manuel.quijada1901@alumnos.ubiobio.cl>
2025-04-11 13:46:15 -0400

    asserts with function from the product

--- Product/test/test_product.py
+++ Product/test/test_product.py
@@ -7,43 +7,38 @@ async def test_success_product(product):
     await product.selectProduct()
     await product.addCart(data.input_success)
     #assert await product.get_field_validation_state("")
-    cart_product = await product.page.locator("#shoppingCart > div > div > div.wrap > div.tf-mini-cart-wrap > div.tf-mini-cart-main > div > div.tf-mini-cart-items > div").is_visible()
-    assert cart_product, "El producto no se encontró en el carrito."
+    assert product.get_information_cart, "El producto no se encontró en el carrito."
 
 @pytest.mark.asyncio
 async def test_fail_infinity_product(product):
     await product.selectProduct()
     await product.addCart(data.input_infinity)
-    cart_product_visible = await product.page.locator("#shoppingCart > div > div > div.wrap > div.tf-mini-cart-wrap > div.tf-mini-cart-main > div > div.tf-mini-cart-items > div").is_visible()
-    assert not cart_product_visible, "El producto no debería estar en el carrito al usar una cantidad infinita."
+    assert not product.get_information_cart, "El producto no debería estar en el carrito al usar una cantidad infinita."
 
 @pytest.mark.asyncio
 async def test_fail_NaN_product(product):
     await product.selectProduct()
     await product.addCart(data.input_Nan)
-    cart_product_visible = await product.page.locator("#shoppingCart > div > div > div.wrap > div.tf-mini-cart-wrap > div.tf-mini-cart-main > div > div.tf-mini-cart-items > div").is_visible()
-    assert not cart_product_visible, "El producto no debería estar en el carrito al usar una cantidad NaN."
+    assert not product.get_information_cart, "El producto no debería estar en el carrito al usar una cantidad NaN."
 
 @pytest.mark.asyncio
 async def test_fail_zero_product(product):
     await product.selectProduct()
     await product.addCart(data.input_zero)
-    cart_product_visible = await product.page.locator("#shoppingCart > div > div > div.wrap > div.tf-mini-cart-wrap > div.tf-mini-cart-main > div > div.tf-mini-cart-items > div").is_visible()
-    assert not cart_product_visible, "El producto no debería estar en el carrito al usar una cantidad Zero."
+    assert not product.get_information_cart, "El producto no debería estar en el carrito al usar una cantidad Zero."
 
 @pytest.mark.asyncio
 async def test_fail_max_product(product):
     await product.selectProduct()
     await product.addCart(data.input_max)
-    cart_product_visible = await product.page.locator("#shoppingCart > div > div > div.wrap > div.tf-mini-cart-wrap > div.tf-mini-cart-main > div > div.tf-mini-cart-items > div").is_visible()
-    assert not cart_product_visible, "El producto se agrega aun con cantidades que deberian tocar el maximo de producto disponible."
+    assert not product.get_information_cart, "El producto se agrega aun con cantidades que deberian tocar el maximo de producto disponible."
 
 @pytest.mark.asyncio
 async def test_fail_neg_product(product):
     await product.selectProduct()
     await product.addCart(data.input_neg)
     await product.get_information_cart()
-    assert not product.cart_product_visible, "El producto no debería estar en el carrito al usar una cantidad negativa."
+    assert not product.get_information_cart, "El producto no debería estar en el carrito al usar una cantidad negativa."
 
 @pytest.mark.asyncio
 async def test_eliminate_compare(product):
@@ -56,12 +51,10 @@ async def test_eliminate_compare(product):
 async def test_int_value(product):
     await product.selectProduct()
     await product.addCart(data.input_int)
-    cart_product_visible = await product.page.locator("#shoppingCart > div > div > div.wrap > div.tf-mini-cart-wrap > div.tf-mini-cart-main > div > div.tf-mini-cart-items > div").is_visible()
-    assert not cart_product_visible, "El producto no debería estar en el carrito al usar una cantidad numeral."
+    assert not product.get_information_cart, "El producto no debería estar en el carrito al usar una cantidad numeral."
 
 @pytest.mark.asyncio
 async def test_empty_product(product):
     await product.selectProduct()
     await product.addCart(data.input_empty)
-    cart_product_visible = await product.page.locator("#shoppingCart > div > div > div.wrap > div.tf-mini-cart-wrap > div.tf-mini-cart-main > div > div.tf-mini-cart-items > div").is_visible()
-    assert not cart_product_visible, "El producto no debería estar en el carrito al usar una cantidad numeral."
+    assert not product.get_information_cart, "El producto no debería estar en el carrito al usar una cantidad numeral."

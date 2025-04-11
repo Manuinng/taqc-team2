5a5fc8f50f271fd0a7b9db8455642fad51fc1020
lfazocar <39388875+lfazocar@users.noreply.github.com>
2025-04-11 14:57:30 -0400

    feat: add session fixture via direct API requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest_asyncio
+import requests
 from typing import Dict, List, Any, AsyncGenerator
 from playwright.async_api import async_playwright, Browser, Cookie
-from config.config import TEST_USER
+from config.config import BASE_URL, TEST_USER
 from pages import AutomationPortal, Navbar, LoginPopup
 
 @pytest_asyncio.fixture(loop_scope="module", scope="module")
@@ -30,7 +31,7 @@ async def browser() -> AsyncGenerator[Browser, None]:
         await browser.close()
 
 @pytest_asyncio.fixture(loop_scope="module", scope="module")
-async def session() -> List[Cookie]:
+async def session_ui() -> List[Cookie]:
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
@@ -49,3 +50,47 @@ async def session() -> List[Cookie]:
         session =  await page.context.cookies()
         await browser.close()
         return session
+
+@pytest_asyncio.fixture(loop_scope="module", scope="module")
+async def session() -> List[Cookie]:
+    session = requests.Session()
+
+    # GET CSRF Token
+    response_csrf = session.get(f"{BASE_URL}/api/auth/csrf")
+    if response_csrf.status_code == 200:
+        csrf_token = response_csrf.json().get('csrfToken')
+    else:
+        raise requests.exceptions.HTTPError(f"Failed to retrieve CSRF token: {response_csrf.status_code}")
+
+    # POST Login
+    data = {
+        "email": TEST_USER["email"],
+        "password": TEST_USER["password"],
+        "redirect": "false",
+        "csrfToken": csrf_token,
+        "callbackUrl": f"{BASE_URL}/login",
+        "json": "true"
+    }
+    response_login = session.post(f"{BASE_URL}/api/auth/callback/credentials", data=data)
+    if response_login.status_code != 200:
+        raise requests.exceptions.HTTPError(f"Login failed: {response_login.status_code}")
+
+    # GET Session
+    response_session = session.get(f"{BASE_URL}/api/auth/session")
+    if response_session.status_code != 200:
+        raise requests.exceptions.HTTPError(f"Failed to retrieve session cookie: {response_session.status_code}")
+
+    context_cookies = []
+    for cookie in session.cookies:
+        context_cookies.append(Cookie(
+            name=cookie.name,
+            value=cookie.value,
+            domain=cookie.domain,
+            path=cookie.path,
+            expires=cookie.expires or -1,
+            httpOnly=cookie.has_nonstandard_attr('HttpOnly'),
+            secure=cookie.secure,
+            sameSite=cookie._rest.get('SameSite', None)
+        ))
+
+    return context_cookies

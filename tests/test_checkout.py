332461cb7b0bfe5bc2efade4ce7ec9ca900a64b6
lfazocar <39388875+lfazocar@users.noreply.github.com>
2025-04-14 22:41:06 -0400

    feat: add invalid test cases to checkout test
    - refactored "test_checkout_required_fields" to
    "test_form_input", which now handles both empty
    and invalid test cases

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -2,22 +2,30 @@ import pytest
 from pages import CheckoutPage
 from playwright.async_api import TimeoutError
 
-required_fields_to_empty = [ None,
-                   "first_name",
-                   "last_name",
-                   "country",
-                   "city",
-                   "address",
-                   "phone",
-                   "email",
-                   "card_number",
-                   "card_date",
-                   "card_cvc",
-                   "tos_checkbox"
-                   ]
-@pytest.mark.parametrize("field_to_empty", required_fields_to_empty)
+@pytest.mark.parametrize("test_field,test_value,test_description", [
+    (None, None, "valid data"),
+    ("first_name", "", "empty first name"),
+    ("last_name", "", "empty last name"),
+    ("country", "", "empty country"),
+    ("city", "", "empty city"),
+    ("address", "", "empty address"),
+    ("phone", "", "empty phone number"),
+    ("email", "", "empty email"),
+    ("card_number", "", "empty card number"),
+    ("card_date", "", "empty card date"),
+    ("card_cvc", "", "empty card cvc"),
+    ("tos_checkbox", False, "unchecked terms and conditions"),
+    ("email", "not_an_email", "invalid email (no @)"),
+    ("phone", "not_a_phone_number", "invalid phone number"),
+    ("card_number", "not_a_card_number", "invalid card number (not a number)"),
+    ("card_number", "0000000000000001", "invalid card number (fails luhn algorithm)"),
+    ("card_date", "not_a_date", "invalid card expiration date (not a date)"),
+    ("card_date", "13/26", "invalid card expiration date (invalid date)"),
+    ("card_date", "12/24", "invalid card expiration date (expired card)"),
+    ("card_cvc", "abc", "invalid card cvc (not a number)"),
+])
 @pytest.mark.asyncio(loop_scope = "module")
-async def test_checkout_required_fields(browser, session, checkout_valid_data, field_to_empty):
+async def test_form_input(browser, session, checkout_valid_data, test_field, test_value, test_description):
     context = await browser.new_context()
     await context.add_cookies(session)
     page = await context.new_page()
@@ -25,7 +33,7 @@ async def test_checkout_required_fields(browser, session, checkout_valid_data, f
     checkout = CheckoutPage(page)
     await checkout.navigate()
     checkout_data = checkout_valid_data.copy()
-    if field_to_empty: checkout_data[field_to_empty] = ""
+    if test_field: checkout_data[test_field] = test_value
     await checkout.fill_billing_details(
         checkout_data["first_name"],
         checkout_data["last_name"],
@@ -52,7 +60,7 @@ async def test_checkout_required_fields(browser, session, checkout_valid_data, f
     except TimeoutError:
         order_placed = False
 
-    if field_to_empty:
-        assert not order_placed, f"Order was placed with required field {field_to_empty} empty"
+    if test_field:
+        assert not order_placed, f"Order was placed with {test_description}"
     else:
         assert order_placed, "Order with valid data wasn't placed after 2s"
